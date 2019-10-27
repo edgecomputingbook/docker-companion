8e888b0a3067fb2673b9209758f11610f608e2c4
Cody Bumgardner <cody@uky.edu>
2019-10-27 17:11:47 -0400

    <commit message>

--- demo/ch2/2.9/build-plugin.sh
+++ demo/ch2/2.9/build-plugin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /opt/edgecomputingbook/demo/mydata
-cd /opt/edgecomputingbook/demo/mydata/filerepo
+cd /opt/edgecomputingbook/demo/mydata/ch2
+cd /opt/edgecomputingbook/demo/mydata/ch2/filerepo
 mvn clean package bundle:bundle
