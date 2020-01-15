0c7d4c96d41bc9bdf5266fd815f0f228600ad161
Cody Bumgardner <cody@uky.edu>
2020-01-15 16:04:57 -0500

    <commit message>

---
+++ demo/ch4/get-plugin.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p /opt/edgecomputingbook/demo/mydata/ch4/prebuilt
+curl -L https://github.com/edgecomputingbook/exampleplugins/blob/master/cepdemo-1.0-SNAPSHOT.jar -o /opt/edgecomputingbook/demo/mydata/ch4/prebuilt/cepdemo-1.0-SNAPSHOT.jar
